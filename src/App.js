8340c254a67a454703bd9e5757dfde7c549c7913
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-28 10:33:41 -0800

    Correct arrow error in redicer/index.js and reducer path in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '/reducers';
+import reducers from './reducers';
 
 class App extends Component {
   render() {
