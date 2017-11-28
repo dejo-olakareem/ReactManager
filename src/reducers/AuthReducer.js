cdf69fe9148ab9963743e8279bf2516009a49827
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-28 15:14:07 -0800

    Write loginUSer function to login user from firebase and import firebase into actio/index.js

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
@@ -17,4 +18,9 @@ export default (state = INITIAL_STATE, action) => {
     default:
     return state;
   }
+};
+
+export const loginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email, password);
+    .then(user => console.log(user));
 };
