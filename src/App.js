5fc3af3506a27abab07919648466c119a07ba7b0
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-28 10:54:29 -0800

    import firebase into  app.js and copied the configuration for webset up and place it inside a component will mount function, changing the var into const so that we can use fire base

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyCMQoq2nniXtYqJ-dVrGgNbJRdEa3nFdxs',
+    authDomain: 'manager-65253.firebaseapp.com',
+    databaseURL: 'https://manager-65253.firebaseio.com',
+    projectId: 'manager-65253',
+    storageBucket: 'manager-65253.appspot.com',
+    messagingSenderId: '908114095727'
+  };
+  firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
