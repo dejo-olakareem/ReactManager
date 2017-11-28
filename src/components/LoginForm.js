7e936d58d23927ffe01d8cce10d74ca165a22c11
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-28 11:54:48 -0800

    Combine Card,CardSection,Button and Input to create login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Log In
+          </Button>
+        </CardSection>
       </Card>
     );
   }
