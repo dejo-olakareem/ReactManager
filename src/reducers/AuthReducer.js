e89f2ebd01dc80ea9b5075125c465315f9c89c51
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-28 13:11:58 -0800

    Create types.js in actions folder to create make action.type constant in action/index.js and all reducers to avoid errors when calling the action

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
-const INITIAL_STATE = {email: ''};
+import { EMAIL_CHANGED } from '../actions/types';
 
-export default ( state = INITIAL_STATE, action) => {
+const INITIAL_STATE = { email: '' };
+
+export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+
     default:
     return state;
   }
