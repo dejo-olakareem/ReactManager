a574bf657df851c40db59cede9e77310155b676b
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-29 11:10:30 -0800

    Render method for  error and style error to show to user if authentication fail, mapped error state to prop also

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Button, Card, CardSection, Input } from './common';
@@ -19,6 +20,18 @@ class LoginForm extends Component {
 
     this.props.loginUser({ email, password });
   }
+
+renderError() {
+  if (this.props.error) {
+    return (
+      <View style={{ backgroundColor: 'white' }}>
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+      </View>
+    );
+  }
+}
 
   render() {
     return (
@@ -42,6 +55,8 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        {this.renderError()}
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Log In
@@ -52,10 +67,20 @@ class LoginForm extends Component {
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
-    password: state.auth.password
+    password: state.auth.password,
+    error: state.auth.error
   };
 };
+
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
