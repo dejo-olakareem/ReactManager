3cf6f344e40a8e737532e5440f063e2a1f43ef53
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-29 20:39:36 -0800

    import Action fro RNRF into action/index and correct camelCase name for employee list

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,7 @@ const RouterComponent = () => {
     <Router>
       <Scene key="root">
         <Scene key="login" component={LoginForm} title="Please Login" />
-        <Scene key="employeelist" component={EmployeeList} title="Employees" />
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
       </Scene>
     </Router>
   );
