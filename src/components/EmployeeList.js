b4c3bb61e4d8c97ca4a9614e7bfb63d3a0dcc6a6
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-06 14:47:41 -0800

    Create lifeCycle components, componentWillMount and finishing componentWillReceive

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    const ds = new ListView.DataSource({
+      rowHAsChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+  }
+
+  componentWillReceiveProps(nextProps) {
+
   }
 
   render() {
