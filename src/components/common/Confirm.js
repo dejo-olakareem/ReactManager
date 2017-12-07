5f210d8b6e18149a7ddf1ff12e9ada52b66ea3d6
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-07 11:57:10 -0800

    Finish constructing the confirm component with styles and other component needed

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,8 +3,48 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { constainerStyle, cardSectionStyle, textStyle } = styles;
 
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequesClose={() => {}}
+    >
+      <View style={constainerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={onAccept}> Yes </Button>
+          <Button onPress={onDecline}> No </Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = {
+  cardSectionStyle:{
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  constainerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
